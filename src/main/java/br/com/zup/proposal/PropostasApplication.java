51f047245fe4519a96f9597cf39dce7334a75e10
andrezza.souza <andrezza.souza@zup.com.br>
2021-08-23 10:45:21 -0300

    Melhorando a visibilidade com healthcheck

--- src/main/java/br/com/zup/proposal/PropostasApplication.java
+++ src/main/java/br/com/zup/proposal/PropostasApplication.java
@@ -1,11 +1,16 @@
 package br.com.zup.proposal;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.reactive.ReactiveManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
+)
 @EnableFeignClients
 public class PropostasApplication {
 
@@ -14,3 +19,4 @@ public class PropostasApplication {
 	}
 
 }
+
