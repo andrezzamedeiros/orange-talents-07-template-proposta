9b785f1a3d8e3e1686cd05ecf5b9991489ea6932
andrezza.souza <andrezza.souza@zup.com.br>
2021-08-24 17:26:21 -0300

    Subindo a aplicacao no docker

---
+++ src/main/java/com/propostas/Propostas/PropostasApplication.java
@@ -0,0 +1,13 @@
+package com.propostas.Propostas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropostasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropostasApplication.class, args);
+	}
+
+}
